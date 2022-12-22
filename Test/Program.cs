5b20e4dc53312d6c961e2849463d18d656f61a8a
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-12-22 11:59:35 +0700

    test

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,12 +1,13 @@
 ﻿using Archlinux.Api;
 using Archlinux.Api.Methods;
+using Archlinux.Api.Types;
 class MainTest
 {
     public async static Task Main()
     {
         ArchlinuxApi archlinuxctx = new ArchlinuxApi();
         var pkgdetails = new PackageDetails(archlinuxctx);
-        await pkgdetails.Get("/packages/core/x86_64/coreutils/json/");
+        await pkgdetails.Name("coreutils").Repository(ArchRepository.community).Architecture(Arch.x86_64).get();
     }
 
 }
