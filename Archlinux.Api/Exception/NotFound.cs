3beccfc98b30331c446230850eca580bdde7fc59
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-12-22 08:04:12 +0700

    add notfound exception

---
+++ Archlinux.Api/Exception/NotFound.cs
@@ -0,0 +1,11 @@
+using Archlinux.Api.Types;
+
+namespace Archlinux.Api.Exception 
+{
+    public class NotFound : System.Exception
+    {
+        public NotFound() : base()
+        {
+        }
+    }
+}
