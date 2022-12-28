1679e290ddf54d0e18c46984883b6c45d214a777
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-12-28 14:10:33 +0700

    add constant

---
+++ Archlinux.Api/Constant/Base.cs
@@ -0,0 +1,22 @@
+
+// SPDX-License-Identifier: GPL-2.0
+
+/*
+*  Copyright (C) 2022 Fadhil Riyanto
+*
+*  github.com/fadhil-riyanto/archlinux-api-wrapper.git
+*/
+
+namespace Archlinux.Api.Constant
+{
+
+    class Base
+    {
+        public static string BasePackageInformation = "/packages";
+
+        public static string BasePackageQuery = "/packages/search/json";
+
+
+
+    }
+}
