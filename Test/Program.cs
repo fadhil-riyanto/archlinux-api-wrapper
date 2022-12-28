6b5a7f95d8fb2f7efc74b4847f7f26d31345a3f3
fadhil-riyanto <id.fadhil.riyanto@gmail.com>
2022-12-28 18:30:09 +0700

    test

--- Test/Program.cs
+++ Test/Program.cs
@@ -7,12 +7,19 @@ class MainTest
     public async static Task Main()
     {
         ArchlinuxApi archlinuxctx = new ArchlinuxApi();
-        PackageDetails pkgdetails = new PackageDetails(archlinuxctx);
-        PackageDetailAll res = await pkgdetails.Name("linux").Repository(ArchRepository.core).Architecture(Arch.x86_64).GetFiles(true).get();
+        // PackageDetails pkgdetails = new PackageDetails(archlinuxctx, usefiles: false);
+   
+        // PackageDetailAll res = await pkgdetails.Name("linux").Repository(ArchRepository.core).Architecture(Arch.x86_64).get();
 
 
         // Console.WriteLine(res.pkgname);
         // Console.WriteLine(res.pkgbase);
-        Console.WriteLine(res.files_count);
+        // Console.WriteLine(res.pkgdesc);
+
+
+        PackageSearch pkgsearch = new PackageSearch(archlinuxctx);
+        PackageSearchResult pkgres = await pkgsearch.Query("coreutils").Repository(ArchRepository.core).get();
+
+        Console.WriteLine(pkgres.results[0].pkgdesc);
     }
 }
